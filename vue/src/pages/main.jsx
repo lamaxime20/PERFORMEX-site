31120cb8deb54214cf5c650e03ceadb0ceae6c03
Lamaxime <lamaxime20@gmail.com>
2025-12-29 21:20:45 +0100

    Fin de la NavBar

--- vue/src/pages/main.jsx
+++ vue/src/pages/main.jsx
@@ -1,11 +1,14 @@
 import NavBar from "../components/navBar";
+import { ACCEUIL, SERVICES, ABOUT } from "../components/navBar";
 
 function Main({activePage}) {
     return (
         <div className="main-root">
             <NavBar activePage={activePage} />
             <div className="main-content">
-                {activePage}
+                {activePage === ACCEUIL && <h1>Bienvenue sur la page d'accueil</h1>}
+                {activePage === SERVICES && <h1>Bienvenue sur la page des services</h1>}
+                {activePage === ABOUT && <h1>Bienvenue sur la page d'à propos</h1>}
             </div>
         </div>
     )
