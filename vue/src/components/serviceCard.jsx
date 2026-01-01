604e9f4b3f9cb7380970ec699a23932f78b7a66b
Lamaxime <lamaxime20@gmail.com>
2026-01-01 08:34:44 +0100

    Début service

---
+++ vue/src/components/serviceCard.jsx
@@ -0,0 +1,46 @@
+export const MARKETING = "marketing";
+export const INNOVATION = "innovation";
+export const INDUSTRIEL = "genie industriel";
+
+function ServiceCard({
+        title, typeService, reverse, description,
+        avantage1, avantage2, avantage3, price, imgSrc
+    }) {
+    return (
+        <div className="serviceCard-root">
+            <header className="serviceCard-header">
+                <h2>{title}</h2>
+                <div className={`serviceCard-type ${
+                    typeService === MARKETING ? "serviceCard-marketing" :
+                    typeService === INNOVATION ? "serviceCard-innovation" :
+                    typeService === INDUSTRIEL ? "serviceCard-industriel" : ""
+                }`}>
+                    {typeService}
+                </div>
+            </header>
+            <main className={`serviceCard-main ${reverse ? "serviceCard-reverse" : "serviceCard-normal"}`}>
+                <div className="serviceCard-Info">
+                    <h4>{description}</h4>
+                    <p>Dans le but de :</p>
+                    <ul>
+                        <li>{avantage1}</li>
+                        <li>{avantage2}</li>
+                        <li>{avantage3}</li>
+                    </ul>
+                    <p>Le tout à</p>
+                    <p className="serviceCard-price">{price}</p>
+                </div>
+                <div className="serviceCard-Image">
+                    <img src={imgSrc} alt="Service Illustration" />
+                    <button 
+                        className="serviceCard-actionButton"
+                    >
+                        Action Button
+                    </button>
+                </div>
+            </main>
+        </div>
+    )
+}
+
+export default ServiceCard;
---
+++ vue/src/components/services.jsx
@@ -0,0 +1,15 @@
+function Services() {
+    return (
+        <div className="services-root">
+            <header className="services-header">
+                <h1>Our Services</h1>
+                <p>We offer a wide range of services to meet your needs.</p>
+            </header>
+            <main className="services-main">
+                
+            </main>
+        </div>
+    )
+}
+
+export default Services;
