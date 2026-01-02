b3759476f2d8f545b6193d783b57e3d61ec8bc58
Lamaxime <lamaxime20@gmail.com>
2026-01-02 13:58:35 +0100

    Squashed commit of the following:
    commit 6c3d1b262db75ada93d17a7027894ed40443378a
    Author: Lamaxime <lamaxime20@gmail.com>
    Date:   Fri Jan 2 13:57:48 2026 +0100
    
        Ajout du mode sombre sur About et ajout du breakpoint 1000px à la sidebar
    
    commit ef79f149697c1778ae29645fd32069a081a99eb0
    Author: Lamaxime <lamaxime20@gmail.com>
    Date:   Fri Jan 2 13:51:27 2026 +0100
    
        About terminé
    
    commit 0383d7b1549c61260755286938e4bb1064276d18
    Author: Lamaxime <lamaxime20@gmail.com>
    Date:   Fri Jan 2 00:42:24 2026 +0100
    
        HTML LIKE de About terminé

---
+++ vue/src/components/about.jsx
@@ -0,0 +1,56 @@
+import LinkCard from "./linkCard";
+import whatsappLogo from '../assets/images/links/whatsappLogo.svg';
+import linkedinLogo from '../assets/images/links/linkedinLogo.svg';
+import emailLogo from '../assets/images/links/emailLogo.svg';
+import { WHATSAPP, LINKEDIN, EMAIL } from "./linkCard";
+import '../assets/styles/about.css'
+
+function About() {
+    return (
+        <div className="about-root">
+            <header className="about-header">
+                <h1>A propos de nous</h1>
+            </header>
+            <main className="about-main">
+                <section className="about-histoire">
+                    <h2>PERFORM-EX</h2>
+                    <div className="about-description">
+                        <p>
+                            PERFORM-EX est bien plus qu'un cabinet de conseil ; nous sommes les architectes de votre performance globale. Nés de la convergence entre le marketing stratégique, l'innovation produit et l'excellence industrielle, nous offrons une approche holistique pour propulser votre entreprise vers de nouveaux sommets.
+                        </p>
+                        <p>
+                            Notre expertise s'articule autour de la conquête de marché et de l'excellence opérationnelle. Grâce à nos études de marché précises et nos campagnes publicitaires ciblées, nous identifions vos opportunités et amplifions votre voix. Parallèlement, nous optimisons votre outil de production par l'aménagement industriel et la standardisation des procédés, transformant vos ateliers en modèles d'efficacité.
+                        </p>
+                        <p>
+                            Nous croyons également que le capital humain est votre première richesse. C'est pourquoi nos programmes de formation spécialisés arment vos équipes des meilleures pratiques en gestion, production et innovation. Choisir PERFORM-EX, c'est choisir un partenaire dévoué à maximiser votre rentabilité et à concrétiser votre vision.
+                        </p>
+                    </div>
+                </section>
+                <section className="about-contacts">
+                    <header>
+                        <h2>Contactez-nous</h2>                        
+                    </header>
+                    <main className="about-contacts-links">
+                        <LinkCard
+                            link="https://wa.me/+237683184360"
+                            imgSrc={whatsappLogo}
+                            nameLink={WHATSAPP}
+                        />
+                        <LinkCard
+                            link="https://www.linkedin.com/in/kamdem-maxime-386844397"
+                            imgSrc={linkedinLogo}
+                            nameLink={LINKEDIN}
+                        />
+                        <LinkCard
+                            link="mailto:maxkn2026@gmail.com"
+                            imgSrc={emailLogo}
+                            nameLink={EMAIL}
+                        />
+                    </main>
+                </section>
+            </main>
+        </div>
+    )
+}
+
+export default About;
---
+++ vue/src/components/linkCard.jsx
@@ -0,0 +1,21 @@
+import '../assets/styles/linkCard.css';
+export const WHATSAPP = "whatsapp";
+export const LINKEDIN = "linkedin";
+export const EMAIL = "email";
+
+function LinkCard({link, imgSrc, nameLink}) {
+    return (
+        <a
+            className={`linkCard-root ${
+                nameLink === WHATSAPP ? "linkCard-whatsapp" :
+                nameLink === LINKEDIN ? "linkCard-linkedin" :
+                nameLink === EMAIL ? "linkCard-email" : ""
+            }`}
+            href={link}
+        >
+            <img src={imgSrc} alt={nameLink} />
+        </a>
+    )
+}
+
+export default LinkCard;
