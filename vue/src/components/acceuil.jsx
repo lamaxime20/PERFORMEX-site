919d7a9b5c4a98ea1781c29cd08ea52bef42b4ca
Lamaxime <lamaxime20@gmail.com>
2026-01-17 14:38:23 +0100

    Modifications de l'entreprise appliquées

--- vue/src/components/acceuil.jsx
+++ vue/src/components/acceuil.jsx
@@ -17,22 +17,41 @@ function Accueil() {
         <div className="acceuil-root">
             <header className="acceuil-header">
                 <h1>PERFOM-EX Consulting</h1>
-                <h3>Investissons bla bla bla</h3>
+                <h3>Marketing - Innovation - Génie industriel</h3>
             </header>
             <main className="acceuil-main">
                 <div className="acceuil-mission">
                     <img src={logo} alt="logo" />
-                    <h2>Nous aidons les entreprises à maximiser leur performance financière en offrant des solutions d'investissement sur mesure, adaptées aux besoins spécifiques de chaque client.</h2>
+                    <h2>Nous optimisons vos ateliers de production et boostons vos ventes par le marketing digital pour maximiser la rentabilité de votre PME, grâce à notre triple expertise en Génie Industriel, Marketing Stratégique et Innovation Produits.</h2>
                 </div>
                 <div className="acceuil-btn-container">
-                    <button className="acceuil-btn-secondary">Voir nos services</button>
-                    <button className="acceuil-btn-primary">Action Principale</button>
+                    <button 
+                        className="acceuil-btn-secondary"
+                        onClick={() => window.open("/services", "_self")}
+                    >
+                        Voir nos services
+                    </button>
+                    <button 
+                        className="acceuil-btn-primary"
+                        onClick={() => window.open("https://wa.me/+237683184360", "_blank")}
+                    >
+                        Lancer mon projet
+                    </button>
                 </div>
             </main>
             <div className='acceuil-about'>
                 <h2>À propos de nous</h2>
-                <p>PERFOM-EX Consulting est une société de conseil en investissement dédiée à aider les entreprises à atteindre leurs objectifs financiers grâce à des stratégies d'investissement innovantes et personnalisées. Notre équipe d'experts travaille en étroite collaboration avec chaque client pour comprendre ses besoins uniques et développer des solutions qui maximisent la valeur et minimisent les risques.</p>
-                <button className="acceuil-btn-primary">En savoir plus</button>
+                <p>PERFORM-EX est un cabinet de conseil en marketing, innovation et Génie
+                    industriel. Nous aidons les entreprises à prendre
+                    des décisions éclairées à travers nos études et
+                    les accompagnons dans l'amélioration de leurs performances
+                    commerciales et opérationnelles à travers nos services.
+                </p>
+                <button 
+                    className="acceuil-btn-primary"
+                >
+                    En savoir plus
+                </button>
             </div>
             <div className='acceuil-confiance'>
                 <h2>Ils nous font confiance</h2>
@@ -43,7 +62,12 @@ function Accueil() {
                         ))}
                     </div>
                 </div>
-                <button className="acceuil-btn-primary">Rejoignez les</button>
+                <button 
+                    className="acceuil-btn-primary"
+                    onClick={() => window.open("https://wa.me/+237683184360", "_blank")}
+                >
+                    Rejoignez les
+                </button>
             </div>
         </div>
     )
